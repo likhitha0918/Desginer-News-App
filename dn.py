7e968d5cb57049d86cfe0f368188e857385db1f4
Likhitha R <89694994+likhitha0918@users.noreply.github.com>
2023-12-30 19:55:02 +0530

    Update dn.py

--- dn.py
+++ dn.py
@@ -20,7 +20,7 @@ def generate_xml(items):
     for item in items:
         xml_item = ET.SubElement(xml_items, 'item')
         for key in item.keys():
-            if key is 'uid' or key is 'arg':
+            if key == 'uid' or key == 'arg':
                 xml_item.set(key, item[key])
             else:
                 child = ET.SubElement(xml_item, key)
